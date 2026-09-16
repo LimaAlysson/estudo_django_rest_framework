4d66342ada01e589700f79664dd89b1f61f31c97
Alysson Lima <alyssonredes32@gmail.com>
2026-09-16 11:48:05 -0300

    feat:trabalhando com validators(MinLengthValidator)

--- escola/models.py
+++ escola/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 class Estudante(models.Model):
     nome = models.CharField(max_length = 100)
@@ -19,7 +20,7 @@ class Curso(models.Model):
         ('A', 'Avançado'),
     }
 
-    codigo = models.CharField(max_length = 10)
+    codigo = models.CharField(max_length = 10, unique=True, validators=[MinLengthValidator(3)])
     descricao = models.CharField(max_length = 100, blank= False)
     nivel = models.CharField(choices = NIVEL, default = 'B', max_length = 1, blank = False, null = False)
 
--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*9&b-cvc!bw@dhva6-kmg4*2n%o7xc9y3v7@h%yd^fkr0@w0c!'
+SECRET_KEY = os.getenv("SETTINGS_SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

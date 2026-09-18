47a2d472cfdfe359cfbf4070d07fd2904f55213d
Alysson Lima <alyssonredes32@gmail.com>
2026-09-18 11:21:33 -0300

    feat:adicionando os filtros de busca e ordenação

--- setup/settings.py
+++ setup/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'escola',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +136,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
-    
+    'PAGE_SIZE': 20,
 }
